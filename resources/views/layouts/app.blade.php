0ba30375fb3df651720f746ff614deddf76b0f0f
SyahrialHipdi <MHsyahrial6@gmail.com>
2026-06-08 23:26:55 +0700

    pertama

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>{{ $title ?? config('app.name') }}</title>
+
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
+    @livewireStyles
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    {{ $slot }}
+
+    @livewireScripts
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/pages/home/⚡index.blade.php
@@ -0,0 +1,53 @@
+<?php
+
+use Livewire\Component;
+use App\Models\Counter;
+
+new class extends Component {
+    //
+    public Counter $counter;
+
+    public int $value;
+
+    public function render()
+    {
+        return $this->view()->layout('layouts::app')->title('Counter');
+    }
+
+    public function mount()
+    {
+        $this->counter = Counter::firstOrCreate(['id' => 1], ['values' => 0]);
+    }
+
+    public function increment()
+    {
+        $value = $this->counter->value;
+        $value++;
+        $this->counter->update([
+            'value' => $value,
+        ]);
+    }
+
+    public function decrement()
+    {
+        $this->counter->decrement('value');
+        $this->counter->refresh();
+    }
+
+    public function ulang()
+    {
+        $this->counter->update(['value' => 0]);
+        $this->counter->refresh();
+    }
+};
+?>
+
+<div>
+    {{-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius --}}
+    <h1>{{ $counter->value }}</h1>
+    <form wire:submit.prevent="increment">
+        <button type:submit>Increment</button>
+    </form>
+    <button wire:click="decrement" type="button">Decrement</button>
+    <button wire:click="ulang" type="button">RESET!</button>
+</div>
